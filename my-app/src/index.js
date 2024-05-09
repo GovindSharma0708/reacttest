7e5a54da35e3291bd753647b368b74d42024686e
GovindSharma0708 <146430357+GovindSharma0708@users.noreply.github.com>
2024-05-09 11:31:33 +0530

    code

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./index.css";
+const firstBook = {
+author: "James Clear",
+title: "Atomic Habits",
+img: "https://images-na.ssl-images-amazon.com/images/I/81YkqyaFVEL._AC_UL600_SR600,400_.jpg",
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+}
+const secondBook = {
+  author: "Robert Greene",
+  title: "The 48 laws of power",
+  img: "https://images-na.ssl-images-amazon.com/images/I/611X8GI7hpL._AC_UL300_SR300,200_.jpg",
+  
+  }
+
+  
+
+
+
+const BookList = () => {
+  return (
+    <section className="booklist">
+      <Book author={firstBook.author} title={firstBook.title} img={firstBook.img}/>
+      <Book author={secondBook.author} title={secondBook.title} img={secondBook.img}/>
+      
+    </section>
+  );
+};
+
+const Book = (props) => {
+  console.log(props);
+  
+  return (
+    <article className="book">
+      <img
+        src={props.img}
+        alt={props.title}
+      />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
+    </article>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<BookList />);
